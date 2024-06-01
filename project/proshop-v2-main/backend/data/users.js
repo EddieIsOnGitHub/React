efd48c86edb5b58808e69a24bc482d638ca599d0
EddieIsOnGetHub <eduan.vanwaveren@gmail.com>
2024-06-01 21:24:10 +0200

    update files

--- project/proshop-v2-main/backend/data/users.js
+++ project/proshop-v2-main/backend/data/users.js
@@ -11,11 +11,13 @@ const users = [
     name: 'John Doe',
     email: 'john@email.com',
     password: bcrypt.hashSync('123456', 10),
+    
   },
   {
     name: 'Jane Doe',
     email: 'jane@email.com',
     password: bcrypt.hashSync('123456', 10),
+    
   },
 ];
 
